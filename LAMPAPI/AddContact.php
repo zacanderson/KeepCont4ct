5bcbdf4bff77dbf86f47407cb79d9a6d1ce8120b
tristanjp12 <tristanjaramillopritchard@gmail.com>
2021-01-21 12:25:20 -0500

    Added work in progress AddContact.php

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,43 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phoneNumber = $inData["phoneNumber"];
+	$email = $inData["email"];
+
+	$conn = new mysqli("localhost", "NotTheBeast", "WeAdoreCOP4331", "KeepContact");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "insert into ContactInfo (firstName,lastName,phoneNumber,email) VALUES (" . $firstName . ",'" . $lastName . ",'" . $phoneNumber . "," . $email . "')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
