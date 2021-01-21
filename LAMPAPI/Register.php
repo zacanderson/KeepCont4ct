bf70330dbabe8f1ce11cca73ad197bd9961820fd
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-01-21 14:19:09 -0500

    Fixes sql string query to use correct column

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -7,16 +7,14 @@
     if($conn->connect_error){
         returnWithError($conn->connect_error);
 
-
     }else{
-        $sql = "SELECT Login FROM Users where firstName= '". $inData["login"]"'";
+        $sql = "SELECT Login FROM Users WHERE Login= '". $inData["login"]"'";
         $result = $conn->query($sql);
 
         if($result->num_rows > 0){
             returnWithError("Username already exists");
 
         }
-        
     }
 
 
