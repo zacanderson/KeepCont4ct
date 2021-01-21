ff46c2d13083ccee4c4d8b3c900e216ce6628690
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-01-21 14:31:34 -0500

    Fixes sql command - concatenation issue

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -4,15 +4,16 @@
 
     $conn = new mysqli("localhost", "NotTheBeast", "WeAdoreCOP4331", "KeepContact");
 
-    if($conn->connect_error){
+    if($conn -> connect_error){
         returnWithError($conn->connect_error);
 
     }else{
-        $sql = "SELECT Login FROM Users WHERE Login= '". $inData["login"]"'";
+        $sql = "SELECT Login FROM Users WHERE Login= '".$inData["login"]."'";
         $result = $conn->query($sql);
 
         if($result->num_rows > 0){
-            returnWithError("Username already exists");
+            $ret = "Username already exists";
+            returnWithError($ret);
 
         }
     }
@@ -25,7 +26,7 @@
 
 
     function returnWithError( $err ){
-	    $retValue = '"error":"' . $err . '"}';
+	    $retValue = '{"error":"' . $err . '"}';
         sendResultInfoAsJson( $retValue );
         
 	}
