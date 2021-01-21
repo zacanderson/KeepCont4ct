2cc14c6131113bbe7d0dfba411094318fea95c57
tristanjp12 <tristanjaramillopritchard@gmail.com>
2021-01-21 00:17:35 +0000

    adding login.php

---
+++ LAMPAPI/Login.php
@@ -0,0 +1,20 @@
+<?php
+    $inData = getRequestInfo();
+
+    $id = 0;
+    $firstName = "";
+    $lastName = "";
+
+    $conn = new myqli("localhost", "NotTheBeast", "WeAdoreCOP4331", "KeepContact");
+
+    $retValue = '{"id": 0, "firstName": "testing", "error:": "test"}'
+
+
+
+
+
+
+
+
+
+?>
