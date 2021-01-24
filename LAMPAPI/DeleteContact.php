bcebd879e4197b53c13109759519fdff3c1194fe
tristanjp12 <tristanjaramillopritchard@gmail.com>
2021-01-24 18:07:06 -0500

    test

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -3,8 +3,8 @@
 	
 	// stores values from json file into variables
 	$FirstName = $inData["FirstName"];
-	//$LastName = $inData["LastName"];
-	//$UserID = $inData["UserID"];
+	$LastName = $inData["LastName"];
+	$UserID = $inData["UserID"];
 
 	$conn = new mysqli("localhost", "NotTheBeast", "WeAdoreCOP4331", "KeepContact");
 	if ($conn->connect_error) 
@@ -14,7 +14,7 @@
 	else
 	{
 		// deletes data from json file into database
-		$sql = "DELETE FROM ContactInfo where firstName = 'Tristan'";
+		$sql = "DELETE FROM ContactInfo where firstName = '" . $FirstName . "' AND userID = " . $userID . "";
 		if( $result = $conn->query($sql) != TRUE )
 		{
 			returnWithError( $conn->error);
@@ -22,7 +22,7 @@
 		$conn->close();
 	}
 	
-	$retvalue1 = '{"Successfully Deleted":"' . $FirstName . '"}"'
+	$retvalue1 = '{"Successfully Deleted":"' . $FirstName . '","' . $LastName . '"}';
 	sendResultInfoAsJson( $retvalue1 );
 	
 	function getRequestInfo()
