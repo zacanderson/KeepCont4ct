056cf4bae82f6fdc4df8cf87abc3d35d01c509b6
tristanjp12 <tristanjaramillopritchard@gmail.com>
2021-01-24 17:43:25 -0500

    Added new DeletContact ( in progress)

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,45 @@
+<?php
+	$inData = getRequestInfo();
+	
+	// stores values from json file into variables
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$UserID = $inData["UserID"];
+
+	$conn = new mysqli("localhost", "NotTheBeast", "WeAdoreCOP4331", "KeepContact");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		// inserts data from json file into database
+		$sql = "DELETE FROM ContactInfo where firstName = $FirstName AND lastName = $LastName AND userID = $userID";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error);
+		}
+		$conn->close();
+	}
+	
+	$retvalue1 = '{"Successfully Deleted":"' . $FirstName . '","' . $LastName . '"}';
+	sendResultInfoAsJson( $retvalue1 );
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
