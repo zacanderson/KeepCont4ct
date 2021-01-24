437da125bc583196108260aa89c743bd3c1593d7
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-01-24 12:47:58 -0500

    Re-doing editcontact endpoint

--- LAMPAPI/EditContact.php
+++ LAMPAPI/EditContact.php
@@ -1,55 +1,36 @@
 <?php
+
     $inData = getRequestInfo();
 
-    //in order to edit a contact
-    //we need a UserID, and the record 
-    //that we want to change - ID in 
-    //from the ContactInfo table 
-
-    $ID = $inData["ID"];
+    $id = $inData["ID"];
     $firstName = $inData["firstName"];
     $lastName = $inData["lastName"];
     $email = $inData["email"];
     $phoneNumber = $inData["phoneNumber"];
 
-    //connect to database using user and password
-    $conn = new mysqli("localhost", "NotTheBeast", "WeAdoreCOP4331", "KeepContact");
-
-    if($conn -> connect_error){
-
-        returnWithError($conn->connect_error);
-
-    }else{
-
-        $sql = $conn->prepare("UPDATE ContactInfo SET firstName= ?, lastName= ?, phoneNumber= ?, email= ? WHERE ID = ?");
-        $sql = bind_param("ssisi", $firstName, $lastName, $phoneNumber, $email, $ID);
-        $sql->execute();
-
-        return returnWithInfo("Record updated");
+    returnWithError($firstName, $lastName, $id);
 
 
-    }
 
-    function sendResultInfoAsJson( $obj ){
-		header('Content-type: application/json');
-	    echo $obj;
-    }
+
 
     function getRequestInfo(){
         return json_decode(file_get_contents('php://input'), true);
-        
-	}
+    }
+
+    function sendResultInfoAsJson( $obj ){
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithInfo( $firstName, $lastName, $id ){
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
 
     function returnWithError( $err ){
-	    $retValue = '{"error":"' . $err . '"}';
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
         sendResultInfoAsJson( $retValue );
         
     }
-    
-    function returnWithInfo($info){
-		$retValue = '{"ID:" '" . $ID . "',"error":'" . $info . "'}';
-		sendResultInfoAsJson( $retValue );
-	}
-
-
 ?>
