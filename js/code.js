9c65259852e39b310e0912a90542d147ab630c17
Dante  Edmonson <danteedmonson@Dantes-MacBook-Pro.local>
2021-02-18 23:14:40 -0500

    test

--- js/code.js
+++ js/code.js
@@ -248,7 +248,7 @@ function searchContacts() {
 					//document.getElementById("contactSearchResult").innerHTML = "Contact(s) retrieved";
 					var jsonObject = JSON.parse(xhr.responseText);
 
-					const contactInfo = etElementsByTagName('table');
+					const contactInfo = getElementsByTagName('table');
 					const myModal = document.querySelector('#myModal');
 
 
