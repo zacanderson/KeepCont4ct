38720db083ecdce05fc516bef942a15bfdc2a675
zacanderson <zacanderson@pm.me>
2021-02-20 14:04:34 -0500

    testing multiple-parameter last names

--- js/code.js
+++ js/code.js
@@ -200,6 +200,30 @@ function deleteContact(userId) {
 	}
 }
 
+function parseName(input) {
+	var fullName = input || "";
+	var result = {};
+
+	if (fullName.length > 0) {
+		var nameTokens = fullName.match(/[A-ZÁ-ÚÑÜ][a-zá-úñü]+|([aeodlsz]+\s+)+[A-ZÁ-ÚÑÜ][a-zá-úñü]+/g) || [];
+
+		if (nameTokens.length > 3) {
+			result.name = nameTokens.slice(0, 2).join(' ');
+		} else {
+			result.name = nameTokens.slice(0, 1).join(' ');
+		}
+
+		if (nameTokens.length > 2) {
+			result.lastName = nameTokens.slice(-2, -1).join(' ');
+			result.secondLastName = nameTokens.slice(-1).join(' ');
+		} else {
+			result.lastName = nameTokens.slice(-1).join(' ');
+			result.secondLastName = "";
+		}
+	}
+
+	return result;
+}
 
 
 function searchContacts() {
@@ -210,6 +234,7 @@ function searchContacts() {
 
 
 	var srch = document.getElementById("inpt_search").value;
+	srch = parseName(srch);
 	//	document.getElementById("contactSearchResult").innerHTML = "";
 
 
