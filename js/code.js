f44e643ad0036f2d2e782cb1040c8d023fde6177
Dante Edmonson <dedmonson@Knights.ucf.edu>
2021-02-20 01:12:15 -0500

    modify button test

--- js/code.js
+++ js/code.js
@@ -190,7 +190,7 @@ function modifyContact(ID) {
 	var url = urlBase + '/EditContact.' + extension;
 
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
+	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try {
 		xhr.onreadystatechange = function () {
