8be72f5fdd52a0de929952fc9d0563ea29731aac
Dante Edmonson <dedmonson@Knights.ucf.edu>
2021-02-20 00:48:26 -0500

    modify button test

--- js/code.js
+++ js/code.js
@@ -185,7 +185,7 @@ function modifyContact(ID) {
 	var lastName = fullName[1];
 
 
-	var jsonPayload = '{"firstName" : "' + firstName + '", "lastName" : "' + lastName + '", "phoneNumber" : "' + phoneNumber + '", "email" : "' + email + '", "userID" : ' + userId + '"id" : "' + ID + '}';
+	var jsonPayload = '{"firstName" : "' + firstName + '", "lastName" : "' + lastName + '", "phoneNumber" : "' + phoneNumber + '", "email" : "' + email + '", "userID" : ' + userId + '"id" : ' + ID + '}';
 
 	var url = urlBase + '/EditContact.' + extension;
 
